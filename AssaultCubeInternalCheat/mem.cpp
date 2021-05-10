b14afa7f00763b033cafbc735ce1aa286f887177
mertolbekirov <mertol2002@gmail.com>
2021-05-11 00:16:36 +0300

    define internal Patch and Nop funcs

--- AssaultCubeInternalCheat/mem.cpp
+++ AssaultCubeInternalCheat/mem.cpp
@@ -14,3 +14,18 @@ void mem::NopEx(BYTE* dst, unsigned int size, HANDLE hProcess) {
 	PatchEx(dst, nopArray, size, hProcess);
 	delete[] nopArray;
 }
+
+void mem::Patch(BYTE* dst, BYTE* src, unsigned int size) {
+	DWORD oldprotect;
+	VirtualProtect(dst, size, PAGE_EXECUTE_READWRITE, &oldprotect);
+	memcpy(dst, src, size);
+	VirtualProtect(dst, size, oldprotect, &oldprotect);
+}
+
+void mem::Nop(BYTE* dst, unsigned int size) {
+	BYTE* nopArray = new BYTE[size];
+	memset(nopArray, 0x90, size);
+
+	Patch(dst, nopArray, size);
+	delete[] nopArray;
+}
